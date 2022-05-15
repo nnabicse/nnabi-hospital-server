a7d8eea1a856b70810f2d1f5a56473062d9e3598
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-15 16:44:08 +0600

    services api implemented

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect()
-        console.log("DB Connected")
+        const serviceCollection = client.db('nnabi-hospital').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
 
     }
     finally {
