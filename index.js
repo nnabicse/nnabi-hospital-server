b0a7395c21f39230e839140a84cd4ff9d21a8ea2
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-17 00:29:18 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -38,8 +39,9 @@ async function run() {
                 $set: user
             };
 
-            const result = await usersCollection.updateOne(filter, updateDoc, options)
-            res.send(result);
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
 
         })
 
