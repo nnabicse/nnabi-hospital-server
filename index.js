17c8d350be3916536fe20e560b3baede2bd8e169
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-17 03:11:32 +0600

    implemented admin api

--- index.js
+++ index.js
@@ -52,16 +52,32 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         })
-
-        app.put('/user/admin/:email', async (req, res) => {
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: "admin" }
-            };
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        })
 
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result);
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+            if (requesterAccount.role === "admin") {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: "admin" }
+                };
+
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+
+            else {
+                res.status(403).send({ message: "Forbidde" });
+            }
+
 
         })
 
